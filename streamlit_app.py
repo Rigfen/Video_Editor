04bd2caba470fa9f080a0a7df4878fcb4e47f636
Rigfen <Rigfen@gmail.com>
2025-12-07 12:37:43 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from pathlib import Path
+from PIL import Image
+import Panda as Pd
 
-st.title("🎈 My new app")
+st.title("🎈Video Editor")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Lets get Editing!"
 )
+product_image = Image.open(Assets_DIR / "Scotland.png")
+    st.image(product_image, width=600 )
+
+st.file_uploader("Load your file here")
+
